8a9ffc1fb58d3448bc9f5c0cffba385de7b4266d
chkg2a <xshalltearvamps@gmail.com>
2024-02-17 22:27:55 +0530

    yes

---
+++ lua/custom/init.lua
@@ -0,0 +1,17 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+vim.lsp.set_log_level("debug")
+local enable_providers = {
+	"python3_provider",
+}
+for _, plugin in pairs(enable_providers) do
+	vim.g["loaded_" .. plugin] = nil
+	vim.cmd("runtime " .. plugin)
+end
+
+vim.g.python3_host_prog = "/home/xrenne/.pyenv/shims/python3.10"
