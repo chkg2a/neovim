1bcda492ce2834af16914953280e2e5a144715e1
chkg2a <xshalltearvamps@gmail.com>
2024-02-25 13:35:01 +0530

    updated

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,15 +1,18 @@
 local overrides = require "custom.configs.overrides"
 local leet_arg = "leetcode.nvim"
-local ls = require "luasnip"
-local s = ls.snippet
-local t = ls.text_node
+-- local ls = require "luasnip"
+-- local s = ls.snippet
+-- local t = ls.text_node
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
-
 
 ---@type NvPluginSpec[]
 local plugins = {
   -- Override plugin definition options
+  {
+    "folke/twilight.nvim",
+  },
+
   {
     "nvimdev/lspsaga.nvim",
     lazy = false,
@@ -72,6 +75,36 @@ local plugins = {
         alias_format = "%B %-d, %Y",
         -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
         template = nil,
+      },
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        follow_url_func = function(url)
+          -- Open the URL in the default web browser.
+          -- vim.fn.jobstart({"open", url})  -- Mac OS
+          vim.fn.jobstart { "xdg-open", url } -- linux
+        end,
+        wiki_link_func = function(opts)
+          if opts.id == nil then
+            return string.format("[[%s]]", opts.label)
+          elseif opts.label ~= opts.id then
+            return string.format("[[%s|%s]]", opts.id, opts.label)
+          else
+            return string.format("[[%s]]", opts.id)
+          end
+        end,
       },
     },
   },
